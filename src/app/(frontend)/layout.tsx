82fc57f2b184a4b2d3db7a67ce3e10972ddc72c5
Jussi Aalto <jussi.aalto@taitounited.fi>
2025-04-14 15:47:48 +0300

    add sanity files

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,4 +1,4 @@
-import "@/app/globals.css";
+import { SanityLive } from "@/sanity/lib/live";
 
 export default function RootLayout({
   children,
@@ -6,8 +6,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <>
+      {children}
+      <SanityLive />
+    </>
   );
 }
